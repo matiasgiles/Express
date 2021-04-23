a5af4c0ea751f8286c1972fe3b01209fa66c11e0
matiasgiles <matias1giles@gmail.com>
2021-04-23 11:48:00 -0300

    "modulo express clase 1 completo"

--- Prueba.js
+++ Prueba.js
@@ -1,3 +1,27 @@
-//hola, que tal?
+const express = require('express');
+const app = express()
+const path= require('path')
+
+app.listen(3000, ()=> console.log("exito"))
+
+app.get("/home", function (req, res){
+    
+    let htmlPath = path.resolve(__dirname, "./")
+    res.sendFile(htmlPath)
+})
+//esta es una opcion
+
+app.get("/404", function (req, res){
+    res.send("Error página no encontrada")
+})
+/*
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about-us.html"))
+});
+
+ESTA ES OTRA OPCION, MAS UTILIZADA, MAS SIMPLE
+*/
+//Stop code run es control option M
 
 
